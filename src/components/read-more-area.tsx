3485cb6d8858ee68f575620615e45625f6828aae
Immanuel Ehsan <foxeian26@gmail.com>
2024-03-13 17:22:28 +0300

    Fixed Some Styling Issues [v1u8]

--- src/components/read-more-area.tsx
+++ src/components/read-more-area.tsx
@@ -3,7 +3,7 @@ import { ReadMoreContentProps, ReadMoreAreaProps } from '../types'
 
 const ReadMoreContent: React.FC<ReadMoreContentProps> = ({ children, textClassName, textStyle }) => {
   return (
-    <p className={textClassName} style={textStyle}>
+    <p className={textClassName} style={{ display: 'inline', ...textStyle }}>
       {children}
     </p>
   )
@@ -30,7 +30,7 @@ const ReadMoreArea: React.FC<ReadMoreAreaProps> = ({
 
   const textExpander = expanded ? children : text.substring(0, wordsLimit) + '...'
   return (
-    <div className={className} style={{ display: 'flex', flexDirection: 'column', ...style }}>
+    <div className={className} style={{ display: 'black', ...style }}>
       <ReadMoreContent textClassName={textClassName} textStyle={textStyle}>
         {textExpander}
       </ReadMoreContent>
@@ -38,11 +38,14 @@ const ReadMoreArea: React.FC<ReadMoreAreaProps> = ({
         <button
           className={buttonClassName}
           style={{
+            display: 'inline',
             color: 'unset',
             backgroundColor: 'unset',
             border: 'unset',
             textDecoration: 'underline',
             cursor: 'pointer',
+            paddingLeft: '8px',
+            paddingRight: '8px',
             ...buttonStyle,
           }}
           onClick={expanded ? onShowLess : onShowMore}
